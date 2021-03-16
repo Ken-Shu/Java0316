9c8389f1b241172717f05e11fd22acd4c850d1cb
Ken-Shu <73887288+Ken-Shu@users.noreply.github.com>
2021-03-16 20:32:41 +0800

    Update HelloWorld.java

--- src/main/java/com/ocp/day01/HelloWorld.java
+++ src/main/java/com/ocp/day01/HelloWorld.java
@@ -4,6 +4,8 @@ package com.ocp.day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java 的第一堂課!!");
+        System.out.println("Hello Java 的第一堂課!!");
+        System.out.println("Hello Java 的第一堂課!!");
     }
     
 }
