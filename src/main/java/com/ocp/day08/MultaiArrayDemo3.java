61ad2e84ad303da620fb15af16d8d0de5a48b4f3
Ken-Shu <t3636873@yahoo.com.tw>
2021-04-01 23:32:35 +0800

    Hello

--- src/main/java/com/ocp/day08/MultaiArrayDemo3.java
+++ src/main/java/com/ocp/day08/MultaiArrayDemo3.java
@@ -9,19 +9,16 @@ public class MultaiArrayDemo3 {
         
         System.out.println(m3.bmi(hw));
     }
-
+    //00 01
+    //10 11
+    //20 21
     public double bmi(double[][] h) {
         double bmi = 0;
         for(double []x : h){
             double height = x[0];
             double weight = x[1];
             bmi = weight/Math.pow(height/100, 2);
-        } 
-//        for (int i = 0; i < h.length; i++) {
-//            for (int j = 0; j < h[i].length; j++) {
-//                bmi = h[i] / Math.pow(h[i][j] / 100, 2.0);
-//            }
-//        }
+        }
 return bmi;
     }
 }
--- src/main/java/com/ocp/day08/MultaiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultaiArrayDemo4.java
@@ -4,18 +4,23 @@ public class MultaiArrayDemo4 {
 
     public static void main(String[] args) {
         double[][] hw = {{170, 160,180} , {60,45,90}};
+        //00 10
+        //01 11
+        //02 12
        MultaiArrayDemo4 m4 = new MultaiArrayDemo4();
         System.out.println(m4.bmi(hw));
     }
     public double bmi(double [][] hw){
         double bmi = 0;
-        for (double [] h : hw) {
-            double height = h[0];
+            for(int i = 0 ; i<hw.length;i++){
+                for (int j = 0; j < hw[i].length; j++) {
+                    double weight = hw[1][j];
+                    double height = hw[0][j];
+                    bmi = weight/Math.pow(height/100, 2);   
+                }
+                
+            }  
             
-            
-            //bmi = weight/Math.pow(height/100, 2);
-            
-        }
         return bmi;
     }
 }
