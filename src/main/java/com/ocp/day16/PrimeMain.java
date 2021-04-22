8ee1091336578aceab2e0f66349267bb54767a2a
Ken-Shu <t3636873@yahoo.com.tw>
2021-04-22 23:34:35 +0800

    Hello

--- src/main/java/com/ocp/day16/PrimeMain.java
+++ src/main/java/com/ocp/day16/PrimeMain.java
@@ -1,10 +1,21 @@
-
 package com.ocp.day16;
 
 public class PrimeMain {
-    public static void main(String[] args) {
-       
-      
+
+    public static void main(String[] args) { //求質數
+        //如果是質數的話
+        PrimeNumber pr = (n) -> {
+            boolean b = true;
+            if(n == 1) return true;
+           for (int i = 2; i < n; i++) {               
+                   if(n % i == 0){
+                       b = false;
+                       break;
+                   }
+            } 
+           return b;
+        };
+        System.out.println(pr.isPrime(7));
     }
-    
+
 }
